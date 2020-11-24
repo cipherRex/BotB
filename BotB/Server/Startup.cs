80688bc0693e4564581072722f26f38a58f3573f
Matt West <cipherRex@gmail.com>
2020-11-24 13:35:48 -0500

    .

--- BotB/Server/Startup.cs
+++ BotB/Server/Startup.cs
@@ -61,7 +61,7 @@ namespace BotB.Server
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+             
             app.UseHttpsRedirection();
             app.UseBlazorFrameworkFiles();
             app.UseStaticFiles();
